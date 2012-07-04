6b2691c5ed9433adbbf9def21ee1d41aa8acb798
libra guest <tanguydm@gmail.com>
2012-07-04 17:02:21 -0400

    Implémentation du wget et untar. A tester

--- install-addfromserver-plugin.sh
+++ install-addfromserver-plugin.sh
@@ -1,19 +1,23 @@
 #!/bin/sh
 
 if [ -z "$1" ] || [ -z "$2" ]; then
-	echo "Usage: installPlugin VERSION INSTANCE, avec instance = prod ou dev"
+	echo "Usage: install-addfromserver-plugin VERSION INSTANCE"
+    echo "Version = v2.3.a, v2.3.b ... ou master"
+    echo "Instance = prod ou dev"
 	exit 1
 fi
 
 # Récupération du .tar.gz depuis le master GitHub
-# TODO: https://github.com/tanguy2m/AddFromServer/tarball/master
+# https://github.com/tanguy2m/AddFromServer/tarball/master
+# Ou pour les versions: https://github.com/tanguy2m/AddFromServer/tarball/v2.3.a
+wget -P ~ -O AddFromServer-$1.tar.gz https://github.com/tanguy2m/AddFromServer/tarball/$1
 
-# Dézippage de l'archive dasn un dossier temporaire
-# TODO
+# Dézippage de l'archive directement dans le bon dossier
+tar -zxf ~/AddFromServer-$1.tar.gz -C ~/test
 
-# Copie des fichiers dans le bon dossier Piwigo
-# TODO: à updater
+# TODO: à supprimer
+#cp -r /c/archive/Geek/Piwigo/AddFromServer/$1 /c/www/piwigo-$2/bin/plugins/
+#mv /c/www/piwigo-$2/bin/plugins/$1 /c/www/piwigo-$2/bin/plugins/AddFromServer
 
-cp -r /c/archive/Geek/Piwigo/AddFromServer/$1 /c/www/piwigo-$2/bin/plugins/
-mv /c/www/piwigo-$2/bin/plugins/$1 /c/www/piwigo-$2/bin/plugins/AddFromServer
-chown -R admin:admin /c/www/piwigo-$2/bin/plugins/AddFromServer/
+# TODO: à conserver
+#chown -R admin:admin /c/www/piwigo-$2/bin/plugins/AddFromServer/
