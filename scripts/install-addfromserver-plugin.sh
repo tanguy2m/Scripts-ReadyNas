76ba7dcc8787394dc8672484d09a10c1fc482a2d
libra guest <tanguydm@gmail.com>
2012-08-08 05:13:52 -0400

    #4: RC1

--- scripts/install-addfromserver-plugin.sh
+++ scripts/install-addfromserver-plugin.sh
@@ -10,18 +10,18 @@ fi
 # Récupération du .tar.gz depuis le master GitHub
 # https://github.com/tanguy2m/AddFromServer/tarball/master
 # Ou pour les versions: https://github.com/tanguy2m/AddFromServer/tarball/v2.3.a
-wget -O ~/AddFromServer-$1.tar.gz https://github.com/tanguy2m/AddFromServer/tarball/$1 --no-check-certificate
+wget -O /tmp/AddFromServer-$1.tar.gz https://github.com/tanguy2m/AddFromServer/tarball/$1 --no-check-certificate
 
 # Création du dossier de destination
 mkdir /c/www/piwigo-$2/bin/plugins/AddFromServer
 
 # Dézippage de l'archive directement dans le bon dossier en supprimant le dossier chapeau
-tar -zxf ~/AddFromServer-$1.tar.gz -C /c/www/piwigo-$2/bin/plugins/AddFromServer --strip-components 1
+tar -zxf /tmp/AddFromServer-$1.tar.gz -C /c/www/piwigo-$2/bin/plugins/AddFromServer --strip-components 1
 
 # Changement de propriétaire du dossier de destination
 chown -R admin:admin /c/www/piwigo-$2/bin/plugins/AddFromServer
 
 # Suppression des fichiers temporaires
-rm ~/AddFromServer-$1.tar.gz
+rm /tmp/AddFromServer-$1.tar.gz
 
 echo "FIN"
